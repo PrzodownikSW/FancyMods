25d98ba5c4e08c27797344b98d82a9ec9fe76274
PrzodownikSW <tycjan.fortu@gmail.com>
2026-05-14 11:54:57 +0200

    C# patch for toolbelt

---
+++ SW.BetterToolbelt/Scripts/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace Harmony
+{
+    public class Entry : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            HarmonyLib.Harmony harmony = new HarmonyLib.Harmony(_modInstance.Name);
+            Log.Out(" Loading Patch: " + this.GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ SW.BetterToolbelt/Scripts/Toolbelt_Patch.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+public class Toolbelt_Patch
+{
+    [HarmonyPatch(typeof(Inventory), "PUBLIC_SLOTS_PLAYMODE", MethodType.Getter)]
+    public class Patch_Inventory_PUBLIC_SLOTS_PLAYMODE
+    {
+        private static bool Prefix(Inventory __instance, ref int __result)
+        {
+            __result = 12;
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Inventory), "SHIFT_KEY_SLOT_OFFSET", MethodType.Getter)]
+    public class Patch_Inventory_SHIFT_KEY_SLOT_OFFSET
+    {
+        private static bool Prefix(Inventory __instance, ref int __result)
+        {
+            __result = 12;
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerActionsLocal), "InventorySlotWasPressed", MethodType.Getter)]
+    public class Patch_PlayerActionsLocal_InventorySlotWasPressed
+    {
+        private static bool Prefix(PlayerActionsLocal __instance, ref int __result)
+        {
+            if (Input.GetKey(KeyCode.Minus))
+            {
+                __result = 10;
+
+                return false;
+            }
+
+            if (!Input.GetKey(KeyCode.Equals))
+                return true;
+
+            __result = 11;
+
+            return false;
+        }
+    }
+}
